0db782f75d25350b2fed3ef59c2db5e983d0f7f0
Emre Erdem <emreerdem502@gmail.com>
2024-12-28 20:05:50 +0300

    cors changed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const apiRouter = require("./app_api/routes/index");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
