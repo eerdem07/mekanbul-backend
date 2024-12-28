6d014b9b843fb74cd22526debb513f96879a9ba7
Emre Erdem <emreerdem502@gmail.com>
2024-12-28 17:38:25 +0300

    cors added

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const apiRouter = require("./app_api/routes/index");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
