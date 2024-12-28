c8f423e6c4266870b76bdda68a4b9ced3be341b7
Emre Erdem <emreerdem502@gmail.com>
2024-12-28 20:14:14 +0300

    cors updated

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ const apiRouter = require("./app_api/routes/index");
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
